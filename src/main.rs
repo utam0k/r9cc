3dfb0ff567cdf920373c408c85a1b80713a9df34
utam0k <k0ma@utam0k.jp>
2018-08-20 16:26:18 +0900

    Add a tokenizer to allow whitespace between tokens.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,68 @@ extern crate r9cc;
 use r9cc::strtol;
 
 use std::env;
+use std::process::exit;
+
+enum TokenType {
+    Num, // Number literal
+}
+
+// Token type
+#[derive(Default, Debug)]
+struct Token {
+    ty: i32, // Token type
+    val: i32, // Number literal
+    input: String, // Token string (for error reporting)
+}
+
+fn tokenize(mut p: String) -> Vec<Token> {
+    // Tokenized input is stored to this vec.
+    let mut tokens: Vec<Token> = vec![];
+
+    let org = p.clone();
+    while let Some(c) = p.chars().nth(0) {
+        // Skip whitespce
+        if c.is_whitespace() {
+            p = p.split_off(1); // p++
+            continue;
+        }
+
+        // + or -
+        if c == '+' || c == '-' {
+            let token = Token {
+                ty: c as i32,
+                input: org.clone(),
+                ..Default::default()
+            };
+            p = p.split_off(1); // p++
+            tokens.push(token);
+            continue;
+        }
+
+        // Number
+        if c.is_ascii_digit() {
+            let (n, mut remaining) = strtol(&p);
+            p = remaining;
+            let token = Token {
+                ty: TokenType::Num as i32,
+                input: org.clone(),
+                val: n.unwrap() as i32,
+            };
+            tokens.push(token);
+            continue;
+        }
+
+        eprint!("cannot tokenize: {}\n", p);
+        exit(1);
+    }
+    return tokens;
+}
+
+fn fail(tokens: &Vec<Token>, i: usize) {
+    eprint!("unexpected character: {:?}\n", tokens[i]);
+    exit(1);
+}
+
 
 fn main() {
     let mut args = env::args();
@@ -10,37 +72,46 @@ fn main() {
         return;
     }
 
-    let p = args.nth(1).unwrap();
+    let tokens = tokenize(args.nth(1).unwrap());
 
+    // Print the prologue
     print!(".intel_syntax noprefix\n");
     print!(".global main\n");
     print!("main:\n");
 
+    // Verify that the given expression starts with a number,
+    // and then emit the first `mov` instruction.
+    if tokens[0].ty != TokenType::Num as i32 {
+        fail(&tokens, 0);
+    }
+    print!("  mov rax, {}\n", tokens[0].val);
 
-    let (n, mut p) = strtol(&p);
-    print!("  mov rax, {}\n", n.unwrap());
-
-    while let Some(c) = p.chars().nth(0) {
-        let s = p.split_off(1);
-
-        if c == '+' {
-            let (n, remaining) = strtol(&s);
-            p = remaining;
-            print!("  add rax, {}\n", n.unwrap());
+    // Emit assembly as we consume the sequence of `+ <number>`
+    // or `- <number>`.
+    let mut i = 1;
+    while i != tokens.len() {
+        if tokens[i].ty == '+' as i32 {
+            i += 1;
+            if tokens[i].ty != TokenType::Num as i32 {
+                fail(&tokens, i);
+            }
+            print!("  add rax, {}\n", tokens[i].val);
+            i += 1;
             continue;
         }
 
-        if c == '-' {
-            let (n, remaining) = strtol(&s);
-            p = remaining;
-            print!("  sub rax, {}\n", n.unwrap());
+        if tokens[i].ty == '-' as i32 {
+            i += 1;
+            if tokens[i].ty != TokenType::Num as i32 {
+                fail(&tokens, i);
+            }
+            print!("  sub rax, {}\n", tokens[i].val);
+            i += 1;
             continue;
         }
 
-        eprint!("unexpected character: {}\n", p);
-        return;
+        fail(&tokens, i);
     }
 
     print!("  ret\n");
-    return;
 }
